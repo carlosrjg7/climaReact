ec642561ffad1c644ec4eeb0a7e1618e7e327794
carlosrjg7 <carlosrjg7@gmail.com>
2020-04-27 03:20:32 -0500

    Updates

---
+++ precache-manifest.75d0f27812e8b812d2eb20f59a72dad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4402ac635e743ca232c7a18e68409f1",
+    "url": "/climaReact/index.html"
+  },
+  {
+    "revision": "5dd4b1c99d4556242f95",
+    "url": "/climaReact/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "852b37680e6f1a8d60ff",
+    "url": "/climaReact/static/css/main.077393be.chunk.css"
+  },
+  {
+    "revision": "5dd4b1c99d4556242f95",
+    "url": "/climaReact/static/js/2.18a4e14c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/climaReact/static/js/2.18a4e14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852b37680e6f1a8d60ff",
+    "url": "/climaReact/static/js/main.74d34916.chunk.js"
+  },
+  {
+    "revision": "de85facbf4a23f7bd6cc",
+    "url": "/climaReact/static/js/runtime-main.3887ed54.js"
+  }
+]);
